305d8dc519705214d2007db07300e59362bbd06c
6704062616010 <s6704062616010@email.kmutnb.ac.th>
2024-11-28 18:47:41 +0700

    input n1 n2

--- lab1minx.c
+++ lab1minx.c
@@ -3,9 +3,9 @@ int main() {
 	int num1;
 	int num2;
 	printf("n1;");
-	scanf("?",&num1)
+	scanf("%d",&num1)
 	printf("n2;");
-	scanf("?",&num2)
+	scanf("%d",&num2)
 	if (num1>num2){
 		printf("num1 is greater than num2");
 		//diff of num1 and num2

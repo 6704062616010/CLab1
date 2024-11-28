9bd65ce3c0dd911f6458c370fce050280c6f3d35
6704062616010 <s6704062616010@email.kmutnb.ac.th>
2024-11-28 18:31:21 +0700

    diff num1 and num2

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,16 @@
-
 #include <stdio.h>
 int main() {
-	int num1=10;
-	int num2=5;
+	int num1;
+	int num2;
+	printf("n1;");
+	scanf("?",&num1)
+	printf("n2;");
+	scanf("?",&num2)
 	if (num1>num2){
 		printf("num1 is greater than num2");
-	}
-	else
+		//diff of num1 and num2
+		printf("\n%d",(num1-num2));
+	}else
 		printf("num2 is greater than num1");
 	}
 }
